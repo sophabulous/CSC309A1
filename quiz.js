8f19ba5433dd51bd9373b634e605bc3cd129227b
Sophie Wang <hereissophie@gmail.com>
2016-10-18 14:51:50 -0400

    added comments

--- quiz.js
+++ quiz.js
@@ -1,35 +1,15 @@
 'use strict';
 
-/* Lab 3: Airline route display. */
 
-/**
- * App name space.
- *
- * The
- * quiz object encapsulates the variables and functions
- * into a namespace to prevent interference with global variables from
- * other libraries. In this case, it isn't strictly necessary, but a good
- * habit to get into.
- *
- * @type {object}
- quiz
- */
 var quiz = quiz || {};
 var score = 0;
 var q1Answered = false;
 var q2Answered = false;
 var q3Answered = false;
 var q4Answered = false;
-/**
- * Indicates which cities have a direct flight between them.
- * A key has a direct flight to each of the cities in the array
- * associated with that key.
- *
- * @type {object} routes
- * @const
- */
 
-
+//dictionaries that keeps the data for each question
+// in quiz.Q1 the names are the keys, and the explanations are values
 quiz.Q1 = {
   "HAL 9000": "Incorrect: The HAL 9000 is a fictional computer from Arthur C. Clarke's 2001: A Space Odyssey.",
   "FERUT": "Correct: The machine arrived in Canada on April 30, 1952.  Named FERUT (FERranti U of T), it was used to compute changes in water levels due to the opening of the St. Lawrence Seaway.",
@@ -39,6 +19,7 @@ quiz.Q1 = {
 
 quiz.Q2 = ["loop", "inline", "keyboard", "interpreter", "function", "block", "character", "variable"];
 
+// in quiz.Q3 use the professor's names as the keys and the fame as values
 quiz.Q3 = {
   "Daniel Wigdor": "Taught a first-year course while an undergraduate student in our department",
   "Stephen Cook": "Turing Award winner for work in computational complexity",
@@ -50,6 +31,7 @@ quiz.Q3 = {
   "Mike Brudno": "Scientific Director of the Centre for Computational Medicine at Sick Kids Hospital"
 };
 
+// in quiz.Q4 user the dates as the keys and the events as the values
 quiz.Q4 = {
   "1940": "The First Computer Network",
   "1971": "First Microprocessor: Intel 4004",
@@ -61,12 +43,13 @@ quiz.Q4 = {
 
 };
 
+//updates the score by updating the score label with the most current score
 quiz.updateScore = function(){
     var scoreLabel = document.getElementById("score");
     scoreLabel.innerHTML = "Score: " + score.toString();
 };
 
-//Fisher-Yates algorithm.
+//Fisher-Yates algorithm for randomizing arrays
 quiz.shuffle = function(sourceArray){
     for (var i = 0; i < sourceArray.length - 1; i++) {
         var j = i + Math.floor(Math.random() * (sourceArray.length - i));
@@ -78,33 +61,51 @@ quiz.shuffle = function(sourceArray){
     return sourceArray;
 };
 
+// build Q1
 quiz.buildQ1 = function() {
+    // get the area with the id "q1form" and append the contents of q1 to it
     let q1Form = document.getElementById("q1form");
+    
+    // iterate through every key of the dictioanry quiz.Q1
     for (let key in quiz.Q1){
+        // create a radio button for each key
         let input = document.createElement("input");
         input.type = "radio";
         input.name = "option"
+        //setting the value of the radio button to the key
         input.value = key;
         input.onclick = function() {
+            // retrieve the p element for holding the explanations 
+            // then store it in answer
             let answer = document.getElementById(this.value);
             if (answer != null) {
+              // if the answer is not null, set the innerHTML of answer
+              // to the explanation that the selected name corresponds to
               answer.innerHTML = quiz.Q1[this.value];
-
+              // if the answer is correct, update the score and check completion 
+              // of the quiz, and set the answer to the colour green
               if (this.value === "FERUT"){
-                score += 1;
+                // if this is the first time this question has been answered,
+                // add 1 to the score
                 answer.style.color = "green";
                 if (!q1Answered) {
+                  score += 1;
                   q1Answered = true;
                   quiz.updateScore();
                   quiz.checkCompletion();
                 }
               }else{
+                // if the answer is incorrect, check completion 
+                // of the quiz, and set the answer to the colour green
                 answer.style.color = "red";
                 if (!q1Answered) {
+                  // if this is the first time the question has been answered, 
+                  // set q1Answered to true
                   q1Answered = true;
                   quiz.checkCompletion();
                 }
               }
+              // hide the explanation of every other option that are not selected 
               for (let option in quiz.Q1){
                 if (option !== this.value) {
                   let otherAnswer = document.getElementById(option);
@@ -113,22 +114,30 @@ quiz.buildQ1 = function() {
               }
             }
         };
-
+        // append the radio button
         q1Form.appendChild(input);
+        // append the names after the radio button
         q1Form.appendChild(document.createTextNode(key));
+        // insert a new line
         let linebreak = document.createElement("br");
+        // append the new line
         q1Form.appendChild(linebreak);
+        // create a p element for the explanation and append it after the new line
         let explanationNode = document.createElement("p");
+        // set the id for each p element to the name
         explanationNode.id = key;
         q1Form.appendChild(explanationNode);
 
     }
-
+    
+    // append the button that displays all explanations
     let submitButton = document.createElement("input");
     submitButton.type = 'button';
     submitButton.value = 'Display All Explanations';
+    
+    // iterate through every name in Q1 and set the innerHTML of
+    // each of their corresponding p elements to their explanations
     submitButton.onclick = function() {
-        q1Answered = true;
         for (let key in quiz.Q1){
           let a = document.getElementById(key);
           a.innerHTML = quiz.Q1[key];
@@ -137,45 +146,60 @@ quiz.buildQ1 = function() {
     q1Form.appendChild(submitButton);
 };
 
-
+// build Q2
 quiz.buildQ2 = function() {
+    // get the area by id "q2form" and append corresponding content to it
     let q2Form = document.getElementById("q2form");
+
+    // iterate through every key of the dictioanry quiz.Q1
     for (let i = 0; i < quiz.Q2.length; i++){
+        // create a checkbox for each key
         let input = document.createElement("input");
         input.type = "checkbox";
         input.name = "word";
         input.value = quiz.Q2[i];
 
+        // apend to checkbox to q2Form
         q2Form.appendChild(input);
+        // append the words after the checkbox
         q2Form.appendChild(document.createTextNode(quiz.Q2[i]));
-        var linebreak = document.createElement("br");
+        // append a new line
+        let linebreak = document.createElement("br");
         q2Form.appendChild(linebreak);
     }
 
+    // create a submit button
     let submitButton = document.createElement("input");
     submitButton.type = 'button';
     submitButton.value = 'Submit';
     submitButton.onclick = function() {
+      // count keeps track of how many elements are checked
       let count = 0; 
+      // create empty array answers
       let answers = [];
+      // iterate through all the elements in q2Form, find the ones that
+      // are checkboxes and store them into answers if they are checked
       for (let i = 0; i < q2Form.elements.length; i++) { 
         let obj = q2Form.elements[i]; 
         if (obj.type == "checkbox" && obj.checked) { 
+          // use the index of the checked element as key and their value as the value
           answers[count] = obj.value;
+          // increment count if the element is a checkbox
           count++; 
         } 
       }
+      // create variable response to hold the element that has the 
       let response = document.getElementById("q2response");
       if (count > 2) {
-        // if more than 2 options are chosen
+        // if more than 2 options are checked
           response.style.color = "orange";
           response.innerHTML = "Only two words can be selected. Please try again.";
       }else if (count < 2) {
-        // if less than 2 options are chosen
+        // if less than 2 options are checked
           response.style.color = "orange";
           response.innerHTML = "Your answer is incomplete.  Please select another word.";    
       }else{
-          // if only 2 options were chosen
+          // if only 2 options were checked
 
           //if both answers are correct
           if (answers[0] === "function" && answers[1] === "variable") {
@@ -213,38 +237,61 @@ quiz.buildQ2 = function() {
           }
       }
     };
+
+    // append the submit button
     q2Form.appendChild(submitButton);
+    // create a p element for the response and append it after the submit button
     let responseNode = document.createElement("p");
+    // set the id for each of the response to the word they are associated with
     responseNode.id = "q2response";
     q2Form.appendChild(responseNode);
 };
 
+// build Q3
 quiz.buildQ3 = function() {
+    // get the areas by the class names "professors" and "fame"
+    // and append corresponding content to them
     var nameField = document.getElementsByClassName("professors")[0];
     var fameField = document.getElementsByClassName("fame")[0];
+    // use option to keep track of the current prof name that user clicked on
     let option = "";
 
+    // create letter index for putting in front of the options
     let index = "a";
+    // use counter to increment the letter index
     let counter = 0;
+    // iterate through all the keys in quiz.Q3
     for (let key in quiz.Q3){
+        // create the a p element that corresponse to the prof's name for each key
         let prof = document.createElement("p");
+        // set the innerHTML of the p element to the letter index and the prof's name
         prof.innerHTML = String.fromCharCode(index.charCodeAt(0) + counter) + ". " + key;
+        // set the id of the p element to the letter index it corresponds to
         prof.id = String.fromCharCode(index.charCodeAt(0) + counter);
         prof.onclick = function() {
+        // set option to the prof name that is clicked on 
             option = this.id;
         };
+        // append the prof to nameField
         nameField.appendChild(prof);
+        // append a line break
         let linebreak = document.createElement("br");
         nameField.appendChild(linebreak);
+        // increment the counter
         counter++;
     }
 
+    // let keys equal to the keys of quiz.Q3 and shuffle it
     let keys = quiz.shuffle(Object.keys(quiz.Q3));
-    // let keys = Object.keys(quiz.Q3);
+
+    // iterate through every key in keys
     for (let i = 0; i < keys.length; i++){
-        let linebreak = document.createElement("br");
+        // create a p element for each fame
         let fame = document.createElement("p");
+        // set the id of the fame to the professor they correspond to
         fame.id = keys[i];
+        // set the innerHTML of the p element to a placeholder where the letter the user selected will be,  
+        // the text of the fame, and a placeholder for all the right answers
         fame.innerHTML = " <span class='placeholder'></span>" + quiz.Q3[keys[i]] + " <span class='q3answer'> </span>";
         fame.onclick = function() {
             //find the placeholder of the current fame and change the text to the letter the use just clicked on
@@ -252,18 +299,29 @@ quiz.buildQ3 = function() {
             placeholder[0].innerHTML = option;
 
             let answerCount = 0;
+            // retrieve all the placeholders inside answerField and store them in all_placeholders
             let all_placeholders = fameField.getElementsByClassName("placeholder");
+            // iterate through every place holder
             for (let i = 0; i < all_placeholders.length; i++) {
+              // if the place holder has been filled, increment answerCount by 1
               if (all_placeholders[i].innerHTML !== "") {
                 answerCount += 1;
               }
             }
-            if (answerCount == keys.length) {
 
+            // if all the placeholders have been filled, check correctness
+            if (answerCount == keys.length) {
+              // retrieve all the fame objects and store them in all_fame
               let all_fame = fameField.getElementsByTagName("p");
+              // recreate all the placeholders for the right answers and store them in answerFields
               let answerFields = fameField.getElementsByClassName("q3answer");
+              // iterate through all the right answer placeholders
               for (let i = 0; i < answerFields.length; i++) {
-                let matchedAnswer = document.getElementById(all_placeholders[i].innerHTML); //the prof the user has chosen
+                // retrieve the prof name that the user has chosen for this paticular 
+                // fame and store it in matchedAnswer
+                let matchedAnswer = document.getElementById(all_placeholders[i].innerHTML); 
+                // if the prof the user has chosen matches the prof the fame corresponde to
+                // show that the answer is correct
                 if (matchedAnswer.innerHTML.indexOf(all_fame[i].id) != -1) {
                   answerFields[i].innerHTML = "<br> Correct!";
                   answerFields[i].style.color = "green";
@@ -272,6 +330,7 @@ quiz.buildQ3 = function() {
                     quiz.updateScore();
                   }
                 }else{
+                  // if the names don't match, show that the answer is incorrect
                   answerFields[i].style.color = "red";
                   answerFields[i].innerHTML = "<br> Incorrect! The correct answer is " + all_fame[i].id;
                 }
@@ -282,58 +341,82 @@ quiz.buildQ3 = function() {
               };
             }
         };
+        // append the fame to fameField
         fameField.appendChild(fame);
-        
+        // append a new line
+        let linebreak = document.createElement("br");
         fameField.appendChild(linebreak);
     }
 
 };
+
+// build Q4
 quiz.buildQ4 = function() {
+    // get the area by the id "q4form" and append corresponding content to it
     let q4Form = document.getElementById("q4form");
+    // let keys be the keys of quiz.Q4 and shuffle it
     let keys = quiz.shuffle(Object.keys(quiz.Q4));
+    // store all the dates in quiz.Q4 in q4dates
     let q4dates = Object.keys(quiz.Q4);
+    // sort the dates so that they are in increasing order
     q4dates.sort();
+
+    // iterate through all the keys
     for (let i = 0; i < keys.length; i++){
+        // for every key create a select drop down element
         let selctionDiv = document.createElement('div');
+        // set the class name for css styling
         selctionDiv.className = "selection";
+        // for each select element, set the id to the date 
         let selectList = document.createElement("select");
         selectList.id = keys[i];
+        // append the select element
         selctionDiv.appendChild(selectList);
 
-        //Create and append the options
+        // Create and append the options. Iterate through all the
+        // keys and use the index as the options
         for (let j = 0; j < keys.length; j++) {
             let option = document.createElement("option");
             option.value = (j + 1).toString();
             option.text = (j + 1).toString();
             selectList.appendChild(option);
         }
+
+        // create and append a span element for the event
         let invention = document.createElement("span");
         invention.innerHTML = quiz.Q4[keys[i]];
         selctionDiv.appendChild(invention);
 
+        // create and append a span element for the 
+        // placeholder of the right answers
         let q4answer = document.createElement("span");
         q4answer.innerHTML = "";
         q4answer.className = "q4answer";
         selctionDiv.appendChild(q4answer);
 
+        // append a newline
         let linebreak = document.createElement("br");
         selctionDiv.appendChild(linebreak);
         q4Form.appendChild(selctionDiv);
     }
     
+    // create the submit button
     let submitButton = document.createElement("input");
     submitButton.type = 'button';
     submitButton.value = 'Submit';
 
+    // retrieve all the select elements and store them in all_selections
     let allSelections = document.getElementsByTagName("select");
     let scoreCount = 0;
+
     submitButton.onclick = function() {
+      // iterate through all the select elements
       for (let z = 0; z < allSelections.length; z++){
         let selection = allSelections[z];
+        // retrieve all the right answer placeholders and store them in answerField 
         let answerFields = q4Form.getElementsByClassName("q4answer");
-        // the index the user has chosen for the current select
+        // get the index of the current select
         let index = parseInt(selection.options[selection.selectedIndex].value);
-
         //if the date associated with the chosen index matches the id of the select element, it's correct
         if (q4dates[index - 1] === selection.id) {
           answerFields[z].innerHTML = "    Correct! The year is " + selection.id;
@@ -344,6 +427,7 @@ quiz.buildQ4 = function() {
           answerFields[z].style.color = "red";
         }
       }
+      // check if the user got all the orders right
       if (scoreCount == allSelections.length) {
         if (!q4Answered) {
           q4Answered = true;
@@ -358,15 +442,21 @@ quiz.buildQ4 = function() {
           }
       }
     };
-    q4Form.appendChild(submitButton);
 
+    // append the submit button
+    q4Form.appendChild(submitButton);
 };
 
+// check the completion of the quiz
 quiz.checkCompletion = function() {
+  // if every question has been answered create alert
   if (q1Answered && q2Answered && q3Answered && q4Answered) {
     setTimeout(function(){
-      var r = confirm("Your score is " + score + ". Restart the game?");
+      // set message of alertbox
+      var r = confirm("Your score is " + score + ". Restart the quiz?");
       if (r == true) {
+        // if the user choose to restart the quiz, set score back to 0 and
+        // all the question boolean variables to false
           score = 0;
           q1Answered = false;
           q2Answered = false;
